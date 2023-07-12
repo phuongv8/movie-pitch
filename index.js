05840cbccd1e6063f385e3abfd55c3faebf91dcb
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-07-11 20:36:11 -0700

    fetch synopsis from api

---
+++ index.js
@@ -0,0 +1,62 @@
+import { process } from './env';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const setupInputContainer = document.getElementById('setup-input-container');
+const movieBossText = document.getElementById('movie-boss-text');
+const outputTextArea = document.getElementById('output-text');
+
+document.getElementById('send-btn').addEventListener('click', () => {
+  const setupTextarea = document.getElementById('setup-textarea');
+
+  const textAreaInput = setupTextarea.value;
+  if (textAreaInput) {
+    setupInputContainer.textContent = `<img src="images/loading.svg" class="loading" id="loading">`;
+    movieBossText.textContent = `Ok, just wait a second while my digital brain digests that...`;
+    // fetchBotReply(textAreaInput);
+    fetchSynopsis(textAreaInput);
+  }
+});
+
+async function fetchBotReply(userInput) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+    ###
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+    ###
+    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+    ###
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${userInput}
+    message: 
+    `,
+    max_tokens: 60,
+  });
+  movieBossText.textContent = response.data.choices[0].text;
+}
+
+async function fetchSynopsis(userInput) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    ###
+    outline: ${userInput}
+    synopsis:
+    `,
+    max_tokens: 700,
+  });
+  outputTextArea.textContent = response.data.choices[0].text;
+}
